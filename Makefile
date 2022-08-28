96cd7c478ed03d37abcbc4a27b179953f27de52d
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-08-29 00:36:46 +0900

    show db data to console

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY
 create_db:
 	sqlite3 db/user.db < db/user.sql
+
+.PHONY
+dev:
+	uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session, SQLModel, create_engine, select
 from user import User
 
 app = FastAPI()
 engine = create_engine("sqlite:///db/user.db")
-SQLModel.metadata.create_all(engine)
-
-# with Session(engine) as session:
-
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    results = None
+    with Session(engine) as session:
+        statement = select(User)
+        results = session.exec(statement)
+        for u in results:
+            print(f"--------------------------")
+            print(f"ID   : {u.id}")
+            print(f"NAME : {u.name}")
+            print(f"type : {type(u)}")
+
+    print(results)
+    return results
